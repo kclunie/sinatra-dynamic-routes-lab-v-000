cce8368708b3f464c8ba4130b0aeb22c9bc00fe3
Kristen Clunie <kcluniesa@yahoo.com>
2019-04-10 00:03:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "#{@phrase}\n" * @number
   end
 
-   get '/say/:number/:phrase' do
+   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
     "#{@phrase}\n" * @number
